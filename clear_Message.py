68fab0ee46e36bcb0c1ee111f8d76b80d7d1ae17
LemonAI <147178284+LemonServer@users.noreply.github.com>
2024-03-22 15:29:43 +0800

    Update clear_Message.py

--- clear_Message.py
+++ clear_Message.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from data import Message
 from context import UserMessageContext
-def clear():
+def clear():#清理会话
   global Message
   now = datetime.now()
   for i in range(10):
     if Message[i].username != "未注册":
-        delta_time = (now - Message[i].register_time).total_seconds()
-        if delta_time > 3600:
+        delta_time = (now - Message[i].register_time).total_seconds() #计算注册到现在的时间差
+        if delta_time > 3600:  #若时间差超过一小时则清理会话
             Message[i] = UserMessageContext(i)
