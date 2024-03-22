134300620482e84ac6fbc8f6fb4c6de63d96e63c
LemonAI <147178284+LemonServer@users.noreply.github.com>
2024-03-22 16:15:35 +0800

    Update data.py

--- data.py
+++ data.py
@@ -5,6 +5,6 @@ import context
 Wx = uiautomation.WindowControl(Name="微信")#微信窗口
 Message = [context.UserMessageContext(i) for i in range(10)]  # 用来储存待处理信息的变量
 password = '@Lemon bot'#群聊中的"@"
-api="d4d3b6869f22136f121d9b6dfc475bc0.ZD9dl84s6ZydwcMj"
+api="请输入api"
 client=ZhipuAI(api_key=api)
 number=-1
